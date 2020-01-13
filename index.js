1f2f8d2bd0c3bcd768663d6075793ace1c0aec3c
lia3air <lia.draier@stud.srh-campus-berlin.de>
2020-01-13 14:48:54 +0100

    These two new lines add a real calculator functionality to the calculator program.

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ var operand1 = arguments[0];
 var operand2 = arguments[1];
 var operator = arguments[2];
 
+operand1 = parseFloat(operand1);
+operand2 = parseFloat(operand2);
+
 console.log('Calculating:', operand1, operand2, operator);
 
 var result;
